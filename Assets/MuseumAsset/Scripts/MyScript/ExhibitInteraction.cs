10be9417b4f583ae45d7b881e5c6e45a956008b7
pollychen5911 <pollychen3911@gmail.com>
2024-07-14 20:29:10 +0800

    點擊物體顯示Canvas旋轉3D模型
    問題:
    旋轉3D模型會連主相機視角一起旋轉

---
+++ Assets/MuseumAsset/Scripts/MyScript/ExhibitInteraction.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class ExhibitInteraction : MonoBehaviour
+{
+    public GameObject[] descriptionCanvases; // 所有展品对应的描述画布数组
+    public Camera mainCamera;
+    public Camera infoCamera;
+    public GameObject manager;
+    public GameObject selectRoomCanvas;
+
+    void Start()
+    {
+        // 初始时隐藏所有描述画布和信息相机
+        foreach (GameObject canvas in descriptionCanvases)
+        {
+            canvas.SetActive(false);
+        }
+        infoCamera.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        Vector3 pos = Input.mousePosition;
+        Ray mouseRay = mainCamera.ScreenPointToRay(pos);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitObj;
+            if (Physics.Raycast(mouseRay, out hitObj))
+            {
+                print(hitObj.collider.gameObject.tag);
+                if (hitObj.collider.CompareTag("poster"))
+                {
+                    string objectName = hitObj.transform.parent.gameObject.name;
+                    print(objectName);
+                    ShowInfoCanvas(objectName);
+                }
+            }
+        }
+    }
+
+    void ShowInfoCanvas(string objectName)
+    {
+        foreach (GameObject canvas in descriptionCanvases)
+        {
+            if (canvas.name == objectName)
+            {
+                canvas.SetActive(true);
+            }
+            else
+            {
+                canvas.SetActive(false);
+            }
+        }
+        selectRoomCanvas.SetActive(false);
+        infoCamera.gameObject.SetActive(true);
+    }
+
+    public void HideInfoCanvas()
+    {
+        foreach (GameObject canvas in descriptionCanvases)
+        {
+            canvas.SetActive(false);
+        }
+        selectRoomCanvas.SetActive(true);
+        infoCamera.gameObject.SetActive(false);
+    }
+}
--- Assets/MuseumAsset/Scripts/MyScript/RotateObject.cs
+++ Assets/MuseumAsset/Scripts/MyScript/RotateObject.cs
@@ -3,46 +3,50 @@ using UnityEngine.UI;
 
 public class RotateObject : MonoBehaviour
 {
-    public float rotationSpeed = 0.5f; // 旋轉速度
+    public float rotationSpeed = 5f; // 旋轉速度
     public float smoothTime = 0.1f;
     public RectTransform controlArea; // 控制旋轉的區域
     public Camera uiCamera;
 
+    private float currentRotationX;
     private float currentRotationY;
+    private float targetRotationX;
     private float targetRotationY;
-    private float rotationVelocity;
+    private float rotationVelocityX;
+    private float rotationVelocityY;
 
     private bool isTouchingControlArea = false;
 
+    void Start()
+    {
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        currentRotationX = currentRotation.x;
+        currentRotationY = currentRotation.y;
+        targetRotationX = currentRotation.x;
+        targetRotationY = currentRotation.y;
+    }
     void Update()
     {
-        if (Input.touchCount > 0){ // 手指
-            Touch touch = Input.GetTouch(0);
-
-            if (touch.phase == TouchPhase.Began){
-                isTouchingControlArea = IsTouchingControlArea(touch.position);
-            }
-
-            if (isTouchingControlArea && touch.phase == TouchPhase.Moved){
-                float deltaX = touch.deltaPosition.x; // 手指移動距離
-                targetRotationY -= deltaX * rotationSpeed;
-            }
-
-            if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled){
-                isTouchingControlArea = false;
-            }
-        } else if (Input.GetMouseButtonDown(0)){ // 滑鼠點擊
+        if (Input.GetMouseButtonDown(0)) // 滑鼠點擊
+        {
             isTouchingControlArea = IsTouchingControlArea(Input.mousePosition);
-        } else if (Input.GetMouseButton(0) && isTouchingControlArea){ // 滑鼠拖動
-            float deltaX = Input.GetAxis("Mouse X"); // 滑鼠移動距離
+        }
+        else if (Input.GetMouseButton(0) && isTouchingControlArea) // 滑鼠拖動
+        {
+            float deltaX = Input.GetAxis("Mouse X"); 
+            float deltaY = -Input.GetAxis("Mouse Y"); 
             targetRotationY -= deltaX * rotationSpeed;
-        } else if (Input.GetMouseButtonUp(0)){ // 滑鼠放開
+            targetRotationX += deltaY * rotationSpeed; 
+        }
+        else if (Input.GetMouseButtonUp(0)) // 滑鼠放開
+        {
             isTouchingControlArea = false;
         }
 
         // 平滑旋轉
-        currentRotationY = Mathf.SmoothDampAngle(currentRotationY, targetRotationY, ref rotationVelocity, smoothTime);
-        transform.rotation = Quaternion.Euler(0f, currentRotationY, 0f);
+        currentRotationX = Mathf.SmoothDampAngle(currentRotationX, targetRotationX, ref rotationVelocityX, smoothTime);
+        currentRotationY = Mathf.SmoothDampAngle(currentRotationY, targetRotationY, ref rotationVelocityY, smoothTime);
+        transform.rotation = Quaternion.Euler(currentRotationX, currentRotationY, 0f);
     }
 
     private bool IsTouchingControlArea(Vector2 screenPosition)
